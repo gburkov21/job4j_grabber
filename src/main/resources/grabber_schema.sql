d47b581fc04cd95cee48311acf4b6017ef949ba7
g.burkov <g.burkov@cheb.eraga.net>
2022-03-21 12:08:26 +0300

    4. Схема таблицы Post [#1731 #277980]

--- src/main/resources/grabber_schema.sql
+++ src/main/resources/grabber_schema.sql
@@ -4,7 +4,7 @@ create table post
 (
     id      serial primary key,
     name    varchar(255),
-    text    varchar(255),
-    link    varchar(255) unique,
+    text    varchar,
+    link    varchar unique,
     created timestamp
 );
